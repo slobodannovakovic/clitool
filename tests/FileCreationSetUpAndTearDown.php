903dc6d579197eec1ec8eda47a74d96e17636ad3
Slobodan Novakovic <slobodan.novakovic@typeqast.com>
2022-11-03 14:17:58 +0100

    fixed bugs in test helper files

--- tests/FileCreationSetUpAndTearDown.php
+++ tests/FileCreationSetUpAndTearDown.php
@@ -5,6 +5,8 @@ namespace MakeFile\Tests;
 
 trait FileCreationSetUpAndTearDown {
 	public function setUp(): void {
+		$this->subDir = 'Controllers';
+
 		$this->fileFullName = 'Test/TestController';
 
 		$this->fullDirPath = \MakeFile\Config::get('base_config')['testingBasePath'];
@@ -14,7 +16,8 @@ trait FileCreationSetUpAndTearDown {
 		if(file_exists($this->fullDirPath)) {
 			\MakeFile\Tests\Helper::cleanTempFilesAndFolders(
 				$this->fullDirPath,
-				$this->fileFullName
+				$this->fileFullName,
+				$this->subDir
 			);
 		}
 	}
--- tests/Helper.php
+++ tests/Helper.php
@@ -4,20 +4,22 @@ declare(strict_types=1);
 namespace MakeFile\Tests;
 
 class Helper {
-	public static function cleanTempFilesAndFolders(string $basePath, string $fullFileName): void {
-		unlink($basePath.'Controllers/'.$fullFileName.'.php');
+	public static function cleanTempFilesAndFolders(string $basePath, string $fullFileName, string $subDir): void {
+		$subDir .= '/';
+
+		unlink($basePath.$subDir.$fullFileName.'.php');
 
 		$fullFileNameArr = explode('/',$fullFileName);
 
 		array_pop($fullFileNameArr);
 
 		foreach($fullFileNameArr as $dir) {
-			if(is_dir($basePath.'Controllers/'.$dir)) {
-				rmdir($basePath.'Controllers/'.$dir);
+			if(is_dir($basePath.$subDir.$dir)) {
+				rmdir($basePath.$subDir.$dir);
 			}
 		}
 
-		rmdir($basePath.'Controllers');
+		rmdir($basePath.$subDir);
 		rmdir($basePath);
 	}
 }
